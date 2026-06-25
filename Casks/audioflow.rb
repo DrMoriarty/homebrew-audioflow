0b25fbb641a96ed3677e7208e9228bb050b8e35a
DrMoriarty <drmoriarty.0@gmail.com>
2026-06-25 11:00:21 +0300

    fix style

--- Casks/audioflow.rb
+++ Casks/audioflow.rb
@@ -1,12 +1,13 @@
 cask "audioflow" do
   arch arm: "arm64", intel: "x86_64"
+
   version "2.0.1"
-  sha256 arm: "d4ab3eeda01488e0e865f70cd93e7c7380784469cdfa8b2d0aad51b0a672c75d",
+  sha256   arm: "d4ab3eeda01488e0e865f70cd93e7c7380784469cdfa8b2d0aad51b0a672c75d",
          intel: "ce4dfa99b6805342496b307a315816b0b072a1f6e77b635935277daa968a8075"
 
   url "https://github.com/DrMoriarty/AudioFlow/releases/download/2.0.1/AudioFlow-#{version}.#{arch}.tar.gz"
   name "AudioFlow"
-  desc "Lightweight, real time system audio equalizer for MacOS."
+  desc "Lightweight, real time system audio equalizer"
   homepage "https://github.com/DrMoriarty/AudioFlow"
 
   depends_on :macos
@@ -14,5 +15,5 @@ cask "audioflow" do
 
   app "AudioFLow.app"
 
-  caveats "If you hust installed blackhole-2ch you should reboot the system to take effect."
+  caveats "If you just installed blackhole-2ch you should reboot the system to take effect."
 end
